69219ca2a5bb1e5053a8936f0439fca3ffa0c7ed
Ryan Davidson <daviryan@aol.com>
2018-04-07 18:44:39 +0100

    Refactor now it creates title links

--- popup.js
+++ popup.js
@@ -1,14 +1,21 @@
+function onWindowLoad() {
 
-function action() {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    let url = tabs[0].url;
-    var a = document.createElement('a');
-    var linkText = document.createTextNode("title");
-    a.appendChild(linkText);
-    a.href = url;
-    document.body.appendChild(a);
-    alert(url);
-  });
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var tab = tabs[0]
+        var url = tab.url;
+        var title = tab.title;
+        var a = document.createElement('a');
+        var linkText = document.createTextNode(title);
+        a.appendChild(linkText);
+        a.title = title;
+        a.href = url;
+        a.id = "url"
+        a.contentEditable = "true"
+        document.body.appendChild(a)
+        a.focus();
+        a.select();
+    });
+
 }
 
-$('#formatLink').click(action());
+window.onload = onWindowLoad;
